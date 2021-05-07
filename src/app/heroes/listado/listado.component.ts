bc295045e0bf4100f6182b8c774ff587c7a82bdc
nels <nels@gmail.com>
2021-05-07 10:10:47 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string []= ['spiderman','ironman','hulk','thor','capitan america'];
+heroeBorrado: string ='';
+
+  borrarHeroe(){
+    
+    this.heroeBorrado=this.heroes.shift() ||'';
+  }
+ 
+}
+
